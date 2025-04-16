3d9b394d21d67546cdcc1f8267ee8d74f13b80a7
theus <theys.dev@gmail.com>
2025-04-16 10:23:32 -0300

    Primeira Classe com Hibernate

---
+++ src/main/java/br/com/fiap/contatos/model/Contato.java
@@ -0,0 +1,66 @@
+package br.com.fiap.contatos.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "T_CONTATO")
+public class Contato {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "T_CONTATOS_SEQ")
+    @SequenceGenerator(
+            name = "T_CONTATOS_SEQ",
+            sequenceName = "T_CONTATOS_SEQ",
+            allocationSize = 1)
+    private Long id;
+    private String nome;
+    private String email;
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                '}';
+    }
+}
