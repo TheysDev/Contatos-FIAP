b630bce243041af0e629949c31959ccb7a38f54c
theus <theys.dev@gmail.com>
2025-04-17 17:56:51 -0300

    Organizando o código com o padrão DAO

---
+++ src/main/java/br/com/fiap/contatos/ContatoApp.java
@@ -0,0 +1,66 @@
+package br.com.fiap.contatos;
+
+import br.com.fiap.contatos.dao.Conexao;
+import br.com.fiap.contatos.dao.ContatoDao;
+import br.com.fiap.contatos.model.Contato;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
+
+public class ContatoApp {
+
+    public static void main(String[] args) {
+
+        //Criação EntityManager
+        EntityManager em = Conexao.getEntityManager();
+
+        //cadastrar(em);
+        //atualizar(em);
+        excluir(em);
+
+
+    }
+
+    public static void cadastrar(EntityManager em){
+        Contato contato = new Contato();
+        contato.setNome("Fabiana Gomes");
+        contato.setEmail("fabiana@email.com");
+        contato.setDataNascimento(LocalDate.of(2000,10,15));
+
+        //Criar uma instância do Dao
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        em.getTransaction().begin();
+        contatoDao.salvar(contato);
+        em.getTransaction().commit();
+    }
+
+    public static void atualizar(EntityManager em){
+        Contato contato = new Contato();
+        contato.setId(3L);
+        contato.setNome("Ana Maria");
+        contato.setEmail("ana.maria@fiap.com.br");
+        contato.setDataNascimento(LocalDate.of(2000,10,15));
+
+        //Criar uma instância do Dao
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        em.getTransaction().begin();
+        contatoDao.atualizar(contato);
+        em.getTransaction().commit();
+    }
+    public static void excluir(EntityManager em){
+        Contato contato = new Contato();
+        contato.setId(2L);
+
+        //Criar uma instância do Dao
+        ContatoDao contatoDao = new ContatoDao(em);
+
+        em.getTransaction().begin();
+        contatoDao.excluir(contato);
+        em.getTransaction().commit();
+    }
+
+}
---
+++ src/main/java/br/com/fiap/contatos/dao/ContatoDao.java
@@ -0,0 +1,28 @@
+package br.com.fiap.contatos.dao;
+
+import br.com.fiap.contatos.model.Contato;
+import jakarta.persistence.EntityManager;
+
+public class ContatoDao {
+
+    private EntityManager em;
+
+    public ContatoDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void salvar(Contato contato) {
+        em.persist(contato);
+    }
+
+    public void atualizar(Contato contato) {
+        em.merge(contato);
+    }
+
+    public void excluir(Contato contato) {
+        Contato contatoExcluir = em.find(Contato.class, contato.getId());
+        em.remove(contatoExcluir);
+    }
+
+
+}
